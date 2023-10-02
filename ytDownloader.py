a5e2104c87bef35e3cedd76aa5abd94207ed2766
kMounika11 <143831269+kMounika11@users.noreply.github.com>
2023-10-02 23:50:17 +0530

    Update ytDownloader.py

--- ytDownloader.py
+++ ytDownloader.py
@@ -1,14 +1,1 @@
-from pytube import YouTube
-from sys import argv
-
-link = argv[1]
-yt = YouTube(link)
-
-print("Title: ", yt.title)
-
-print("View: ", yt.views)
-
-yd = yt.streams.get_highest_resolution()
-
-# ADD FOLDER HERE
-yd.download('./YTfolder')
+print("Hello World")
